f16edda55dd98a2e1485a0203364e3f933e06f00
Wei <jrweizhang@gmail.com>
2013-02-24 21:30:51 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NtuMath::Application.config.secret_token = '1969e6153bdfa1420ec33019017de61cd760111db1e995923e3747b4be83f8789e0b13e01cdfa580495ef104010dbd467c0d1cbc20f6570c33fddcd407b8203d'
