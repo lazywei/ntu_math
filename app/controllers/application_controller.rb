d4e5a73ab3f6a4ea9cc472d70a70566c159045ab
Wei <jrweizhang@gmail.com>
2013-03-11 14:31:49 +0800

    allow cross site access.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :allow_cross_site_access
   def after_sign_in_path_for(resource)
     admin_root_path
+  end
+
+  def allow_cross_site_access
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
   end
 end
